932a2e149d38c717312d6807aacda98a7c3560be
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-11-14 18:45:30 +0900

    Add actions

---
+++ src/actions/index.spec.js
@@ -0,0 +1,25 @@
+import * as actions from './index'
+
+describe('todo actions', () => {
+  it('addTodo should create ADD_TODO action', () => {
+    expect(actions.addTodo('Use Redux')).toEqual({
+      type: actions.ADD_TODO,
+      id: 0,
+      text: 'Use Redux'
+    })
+  })
+
+  it('setVisibilityFilter should create SET_VISIBILITY_FILTER action', () => {
+    expect(actions.setVisibilityFilter('active')).toEqual({
+      type: actions.SET_VISIBILITY_FILTER,
+      filter: 'active'
+    })
+  })
+
+  it('toggleTodo should create TOGGLE_TODO action', () => {
+    expect(actions.toggleTodo(1)).toEqual({
+      type: actions.TOGGLE_TODO,
+      id: 1
+    })
+  })
+})
